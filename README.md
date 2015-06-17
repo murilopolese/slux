d7a48ab8082a2323302a5bdcdaa541fccd9f8534
Murilo Polese <murilopolese@gmail.com>
2015-06-17 23:01:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Slux
+----------------------------------
+
+Slack for linux written on [NW.js](http://nwjs.io/). The app is just an iframed version of http://slack.com. Due to restrictions from Slack, you must click a "Go to Slack.com" link on startup.
+
+## Features
+
+- Avoid close window: hide to tray
+- System tray menu
+- Toggle window visibility with right click on system tray
+
+## Roadmap
+
+- Work around the "Go to Slack.com" link on startup
+- In app preferences
+- Configurable behaviour for close button (close, minimize, hide)
+- Configurable keyboard shortuts
+- Gnome and Plank context menu options
---
+++ main.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var gui = require("nw.gui");
+	var win = gui.Window.get();
+	var tray = new gui.Tray({ title: 'Slack', icon: 'slack.png' });
+	var menu = new gui.Menu();
+	var visible = true;
+
+	menu.append(new gui.MenuItem({ label: 'Show' }));
+	menu.append(new gui.MenuItem({ label: 'Hide' }));
+	menu.append(new gui.MenuItem({ label: 'Quit' }));
+	tray.menu = menu;
+
+	menu.items[ 0 ].click = function() {
+		win.show();
+		visible = true;
+	}
+	menu.items[ 1 ].click = function() {
+		win.hide();
+		visible = false;
+	}
+	menu.items[ 2 ].click = function() {
+		visible = false;
+		win.close( true );
+	}
+
+	tray.on( 'click', function() {
+		if( visible ) {
+			win.hide();
+			visible = false;
+		} else {
+			win.show();
+			visible = true;
+		}
+	})
+
+	win.on( 'close', function() {
+		win.hide();
+		return true;
+	})
+});
